6ce10149b8c7cb9353ee7d2a67d7eb26f8eda6fe
vikas.m <vikas.m@covrize.com>
2022-11-07 10:19:13 +0530

    first commit

---
+++ src/components/home/home.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import './home.css';
+
+const Home = () => {
+    return (
+        <>
+            <div className="home">
+                <h1>Home Page</h1>
+                <div className="button">Logout</div>
+            </div>
+        </>
+    );
+}
+
+export default Home;
